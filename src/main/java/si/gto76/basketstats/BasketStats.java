dd268231c8b05b4dd7faf308487793acf7beefd7
Jure Sorn <sornjure@gmail.com>
2014-07-25 18:45:09 +0200

    Started using intellij idea, so the output dir of resources changed a little. Now compiles nicely, with both idea and my run script.

--- src/main/java/si/gto76/basketstats/BasketStats.java
+++ src/main/java/si/gto76/basketstats/BasketStats.java
@@ -1,15 +1,11 @@
 package si.gto76.basketstats;
 
-import java.io.FileNotFoundException;
-
 import javax.swing.UIManager;
-import javax.swing.UIManager.LookAndFeelInfo;
-
 import si.gto76.basketstats.coreclasses.*;
 import si.gto76.basketstats.swingui.SwinGui;
 
 public class BasketStats {
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) {
 		setLookAndFeel(Conf.LOOK_AND_FEEL);
 		Game game = Conf.getDefaultGame();
 		new SwinGui(game);
@@ -19,7 +15,7 @@ public class BasketStats {
 	private static void setLookAndFeel(String lookAndFeel) {
 		try {
 	        UIManager.setLookAndFeel(lookAndFeel);
-	    } 
+	    }
 	    catch (Exception e) {
 	    }
 	}
